dd23bac3da86ad2d0d46995d7e49d193b73de375
ivan <ivan.migov@gmail.com>
2017-12-06 20:50:38 +0200

    header + leftSideBar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+//
+//
+//
+// const appRoutes: Routes = [
+//   { path: '', component: HomeComponent },
+//   { path: 'users', component: UsersComponent, children: [
+//     { path: ':id/:name', component: UserComponent }
+//   ] },
+//   {
+//     path: 'servers',
+//     // canActivate: [AuthGuard],
+//     canActivateChild: [AuthGuard],
+//     component: ServersComponent,
+//     children: [
+//     { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
+//     { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
+//   ] },
+//   // { path: 'not-found', component: PageNotFoundComponent },
+//   { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
+//   { path: '**', redirectTo: '/not-found' }
+// ];
+//
+// @NgModule({
+//   imports: [
+//     // RouterModule.forRoot(appRoutes, {useHash: true})
+//     RouterModule.forRoot(appRoutes)
+//   ],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule {
+//
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { LeftSideBarComponent } from './components/left-side-bar/left-side-bar.component';
+import { FiltersComponent } from './components/filters/filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoListComponent,
+    LeftSideBarComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
